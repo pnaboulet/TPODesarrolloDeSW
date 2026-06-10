ccaad19417498c6e18e7b2f5f02e79c6cbe41045
Pablo-desktop <pablonaboulet@gmail.com>
2026-06-09 21:53:44 -0300

    Algunas clases para empezar a implementar el patron observer

---
+++ backend/src/main/java/legion501st/backend/notificaciones/EmailObserver.java
@@ -0,0 +1,5 @@
+package legion501st.backend.notificaciones;
+
+public class EmailObserver implements NotificacionObserver {
+    
+}
---
+++ backend/src/main/java/legion501st/backend/notificaciones/PushObserver.java
@@ -0,0 +1,5 @@
+package legion501st.backend.notificaciones;
+
+public class PushObserver implements NotificacionObserver {
+    
+}
